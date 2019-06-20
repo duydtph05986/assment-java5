daa925b80708c5b16a698a543fff501d94c24ce1
duydtph05986 <duydtph05986@fpt.edu.vn>
2019-06-20 16:17:48 +0700

    first commit

---
+++ src/test/java/com/springboot/webbanhang/WebbanhangcobanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.webbanhang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebbanhangcobanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
